2c21ebb9bda0e093df40a0d3e00fd14a5ebbd4df
BAZ1201 <133518371+NicoToken@users.noreply.github.com>
2023-05-17 23:53:38 +0800

    Update bot_arbitrage.py

--- bot_arbitrage.py
+++ bot_arbitrage.py
@@ -1,5 +1,7 @@
 import ccxt
 
+import time
+
 # Inisialisasi exchange Binance
 
 exchange = ccxt.binance({
@@ -30,51 +32,53 @@ def get_spot_price(symbol):
 
 def run_arbitrage(symbol, threshold, quantity):
 
-    future_price = get_future_price(symbol)
+    while True:
 
-    spot_price = get_spot_price(symbol)
+        future_price = get_future_price(symbol)
 
-    price_diff = future_price - spot_price
+        spot_price = get_spot_price(symbol)
 
-    
-
-    if price_diff > threshold:
-
-        # Logika pembelian dan penjualan
-
-        # Implementasikan strategi Anda di sini
-
-        # Misalnya, melakukan pembelian di pasar spot dan menjual di pasar future
+        price_diff = future_price - spot_price
 
         
 
-        # Membeli aset di pasar spot
+        if price_diff > threshold:
 
-        buy_order = exchange.create_market_buy_order(symbol=symbol, quantity=quantity)
+            # Logika pembelian dan penjualan
 
-        print("Order pembelian di pasar spot:", buy_order)
+            # Implementasikan strategi Anda di sini
+
+            # Misalnya, melakukan pembelian di pasar spot dan menjual di pasar future
+
+            
+
+            # Membeli aset di pasar spot
+
+            buy_order = exchange.create_market_buy_order(symbol=symbol, quantity=quantity)
+
+            print("Order pembelian di pasar spot:", buy_order)
+
+            
+
+            # Menjual aset di pasar future
+
+            sell_order = exchange.create_market_sell_order(symbol=symbol, quantity=quantity)
+
+            print("Order penjualan di pasar future:", sell_order)
+
+            
+
+            print("Peluang arbitrase terdeteksi!")
+
+            print("Harga future:", future_price)
+
+            print("Harga spot:", spot_price)
+
+            print("Selisih harga:", price_diff)
 
         
 
-        # Menjual aset di pasar future
-
-        sell_order = exchange.create_market_sell_order(symbol=symbol, quantity=quantity)
-
-        print("Order penjualan di pasar future:", sell_order)
-
-        
-
-        print("Peluang arbitrase terdeteksi!")
-
-        print("Harga future:", future_price)
-
-        print("Harga spot:", spot_price)
-
-        print("Selisih harga:", price_diff)
-
-    else:
-
-        print("Tidak ada peluang arbitrase saat ini.")
+        time.sleep(5)  # Tunggu 5 detik sebelum melakukan pengecekan lagi
 
 # Menjalankan bot dengan simbol, threshold, dan quantity tertentu
 
