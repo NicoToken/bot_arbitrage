1be55ae7a45d4a553124fa44cf23e71ff525c8ed
BAZ1201 <133518371+NicoToken@users.noreply.github.com>
2023-05-31 00:55:13 +0800

    Create cex.py

---
+++ cex.py
@@ -0,0 +1,70 @@
+import ccxt
+
+import time
+
+from termcolor import colored
+
+# Inisialisasi objek Binance dan Indodax
+
+binance = ccxt.binance()
+
+indodax = ccxt.indodax()
+
+# Fungsi untuk mendapatkan harga
+
+def get_price(exchange, symbol):
+
+    ticker = exchange.fetch_ticker(symbol)
+
+    return ticker['ask'] if ticker['ask'] else None
+
+# Simpan semua pasangan yang ingin Anda arbitrase
+
+pairs = ['BTC/IDR', 'ETH/IDR', 'BTC/USDT', 'ETH/USDT']
+
+# Loop infinit untuk memeriksa peluang arbitrase
+
+while True:
+
+    for pair in pairs:
+
+        try:
+
+            # Dapatkan harga dari Binance dan Indodax
+
+            binance_price = get_price(binance, pair)
+
+            indodax_price = get_price(indodax, pair)
+
+            if binance_price and indodax_price:
+
+                # Cek apakah ada peluang arbitrase
+
+                if binance_price > indodax_price:
+
+                    # Hitung persentase arbitrase
+
+                    percentage_arbitrage = ((binance_price - indodax_price) / indodax_price) * 100
+
+                    # Tentukan warna teks berdasarkan persentase arbitrase
+
+                    if percentage_arbitrage < 0:
+
+                        colored_text = colored(f"{pair}: {percentage_arbitrage:.2f}%", "red")
+
+                    else:
+
+                        colored_text = colored(f"{pair}: {percentage_arbitrage:.2f}%", "green")
+
+                    # Tampilkan hasil screning di terminal
+
+                    print(colored_text)
+
+        except Exception as e:
+
+            print(f"Terjadi kesalahan: {e}")
+
+    # Waktu tunggu antara setiap iterasi
+
+    time.sleep(5)  # Interval screning 5 detik
+
