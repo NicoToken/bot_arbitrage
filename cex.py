3fff55a6b4a16a81244beb032c2093076295675f
BAZ1201 <133518371+NicoToken@users.noreply.github.com>
2023-05-31 01:42:35 +0800

    Update cex.py

--- cex.py
+++ cex.py
@@ -4,6 +4,8 @@ import time
 
 from termcolor import colored
 
+import requests
+
 # Inisialisasi objek Binance, Indodax, Tokocrypto, KuCoin, dan Bybit
 
 binance = ccxt.binance()
@@ -15,6 +17,14 @@ tokocrypto = ccxt.tokocrypto()
 kucoin = ccxt.kucoin()
 
 bybit = ccxt.bybit()
+
+# Jumlah dana yang akan digunakan untuk arbitrase (misalnya 1000 USDT)
+
+funds = 1000
+
+# Threshold persentase arbitrase untuk melakukan eksekusi (misalnya 2%)
+
+threshold = 2
 
 # Fungsi untuk mendapatkan pair yang tersedia di semua bursa
 
@@ -35,6 +45,24 @@ def get_price(exchange, symbol):
     ticker = exchange.fetch_ticker(symbol)
 
     return ticker['ask'] if ticker['ask'] else None
+
+# Fungsi untuk mengeksekusi tindakan arbitrase
+
+def execute_arbitrage(pair, min_price, max_price):
+
+    # Hitung jumlah aset yang dapat dibeli berdasarkan dana yang tersedia
+
+    amount = funds / min_price
+
+    # Eksekusi tindakan arbitrase
+
+    # Contoh: Beli di Indodax, Jual di Binance
+
+    indodax.create_limit_buy_order(pair, amount, min_price)
+
+    binance.create_limit_sell_order(pair, amount, max_price)
+
+    # Catat atau lakukan tindakan lain yang diperlukan
 
 # Dapatkan semua pair yang tersedia di semua bursa
 
@@ -66,13 +94,10 @@ while True:
 
                 min_price = min(indodax_price, tokocrypto_price, kucoin_price, bybit_price)
 
-                max_price = max(binance_price, indodax_price, tokocrypto_price, kucoin_price, bybit_price)
+                max_price = max(binance_price, indodax_price, tokocrypto_price, kucoin, bybit_price)
+                         # Hitung persentase arbitrase
 
-                percentage_arbitrage = ((max_price - min_price) / min_price) * 100
-
-            else:
-
-                percentage_arbitrage = 0.0
+            percentage_arbitrage = ((max_price - min_price) / min_price) * 100
 
             # Tentukan warna teks berdasarkan persentase arbitrase
 
@@ -84,15 +109,26 @@ while True:
 
                 colored_text = colored(f"{pair}: {percentage_arbitrage:.2f}%", "green")
 
-            # Tampilkan hasil screning di terminal
+            # Tampilkan hasil screening di terminal
 
             print(colored_text)
+
+            # Eksekusi tindakan arbitrase jika persentase arbitrase melebihi threshold
+
+            if percentage_arbitrage > threshold:
+
+                print(f"Terjadi peluang arbitrase yang layak untuk dieksekusi pada pair {pair}!")
+
+                # Eksekusi tindakan arbitrase
+
+                execute_arbitrage(pair, min_price, max_price)
+
+                # Catat atau lakukan tindakan lain yang diperlukan setelah eksekusi
 
         except Exception as e:
 
             print(f"Terjadi kesalahan: {e}")
 
-    # Waktu tunggu antara setiap iterasi
+# Waktu tunggu antara setiap iterasi
 
-    time.sleep(5)  # Interval screning 5 detik
-
+time.sleep(5)  # Interval screning 5 detik
